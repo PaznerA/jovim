4b87c7261b21d6fa3f4ce0a443d8e9f14a64366b
ales.pazner <pazny.develop@gmail.com>
2017-11-22 23:26:52 +0100

    license update

--- src/config/config-dev.php
+++ src/config/config-dev.php
@@ -4,7 +4,7 @@ define('db_driver','mysqli');
 define('db_host','localhost');
 define('db_username','homestead');
 define('db_password','secret');
-define('db_database','dbname');
+define('db_database','superlook_cz');
 define('db_charset','utf8');
-define('db_prefix','35133_');
+define('db_prefix','deine_');
 
--- src/config/config-prod.php
+++ src/config/config-prod.php
@@ -1,7 +1,14 @@
 <?php
+define("APP_PATH", $_SERVER["DOCUMENT_ROOT"]."/www/vivanis/new/");
+define("DATA_PATH", APP_PATH."feed/");
 
-$config=array(
-    "dbPrefix"=>"g9rqb_",
-    "sourceNames"=>array("sperky","parfemy")
-);
+$options = [
+    'driver'   => 'mysqli',
+    'host'     => 'localhost',
+    'username' => 'roloi.cz',
+    'password' => 'i3Sxj3jaX',
+    'database' => 'superlook_cz',
+    'charset'  => 'utf8',
+    'prefix'   => 'g9rqb_'
+];
 
